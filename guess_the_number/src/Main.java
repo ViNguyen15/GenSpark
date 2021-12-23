271e9fafe60241d92a5cb125b21c1e32e83ae8e4
Vi Nguyen <fluffywin@gmail.com>
2021-12-23 10:19:17 -0500

    clean up project 2

--- guess_the_number/src/Main.java
+++ guess_the_number/src/Main.java
@@ -3,22 +3,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input1 = new Scanner(System.in);
+        Scanner input2 = new Scanner(System.in);
         Random random = new Random();
         String toContinue = "y";
 
         System.out.println("Hello! What is your name?");
-        String name = input.nextLine();
+        String name = input1.nextLine();
 
         while(toContinue.equals("y")) {
             System.out.println("Well, " + name + " I am thinking of a number between 1 and 20");
             int answer = random.nextInt(20) + 1;
-            System.out.println(answer);
+            //System.out.println(answer);
 
             int count = 0;
             while (count < 6) {
                 System.out.println("Take a guess");
-                int playerChoice = input.nextInt();
+                int playerChoice = input1.nextInt();
                 if (playerChoice > answer)
                     System.out.println("Your guess is too high.");
                 if (playerChoice < answer)
@@ -34,7 +35,7 @@ public class Main {
                 System.out.println("You tried too many times, the answer is " + answer);
 
             System.out.println("Would you like to play again? (y or n)");
-            toContinue = input.next();
+            toContinue = input2.next();
         }
 
     }
