ca5ad26a6cce4308cfaae13f7fd41e1acd0a7146
Vi Nguyen <fluffywin@gmail.com>
2021-12-28 10:51:27 -0500

    dragon cave test implemented

--- dragon_cave/src/java/Main.java
+++ dragon_cave/src/java/Main.java
@@ -4,54 +4,56 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void startGame(){
+    public void startGame(){
+
+        System.out.println(intro());
+
         Scanner input = new Scanner(System.in);
         try{
-            theChoice(input.nextInt());
+            System.out.println( getChoice( input.nextInt() ) );
         }catch (InputMismatchException e){
-            System.out.println("Not a number");
+            System.out.println("Error: Not a number");
         }
     }
 
-    public static void theChoice(int choice){
-        if(choice!=1 && choice!=2)
-            isLost();
+    public String getChoice(int choice){
         if (choice == 1)
-            evilDragon();
+            return evilDragon();
         if (choice == 2)
-            goodDragon();
+            return goodDragon();
+        return isLost();
     }
 
-    public static void intro(){
-        System.out.println("\nYou are in a land full of dragons. In front of you,\n" +
+    public String intro(){
+        return ("\nYou are in a land full of dragons. In front of you,\n" +
                 "you see two caves. In one cave, the dragon is friendly\n" +
                 "and will share his treasure with you. The other dragon\n" +
                 "is greedy and hungry and will eat you on sight.\n" +
                 "Which cave will you go into? (1 or 2)\n");
     }
 
-    public static void goodDragon(){
-        System.out.println("\nYou approach the cave...\n" +
+    public String goodDragon(){
+        return ("\nYou approach the cave...\n" +
                 "You hear a loud thundering roar follow by a large intimidating silhouette...\n" +
                 "You then hear a loud hearty laughter as the silhouette turns into a dragon...\n" +
                 "The dragon then asked you if you wanted to be friends and offers a golden grail as a sign of your friendship.");
     }
 
-    public static void evilDragon(){
-        System.out.println("\nYou approach the cave...\n" +
+    public String evilDragon(){
+        return ("\nYou approach the cave...\n" +
                 "It is dark and spooky...\n" +
                 "A large dragon jumps out in front of you! He opens his jaws and...\n" +
                 "Gobbles you down in one bite!\n");
     }
 
-    public static void isLost(){
-        System.out.println("\nYou made an invalid choice...\n" +
+    public String isLost(){
+        return ("\nYou made an invalid choice...\n" +
                 "You wander around endlessly in the dungeon...\n" +
                 "You became hungry and start hallucinating...\n" +
                 "As you collapse from exhaustion, your eyes slowly closes seeing a large silhouette laughing at you.");
     }
 
     public static void main(String[] args) {
-        startGame();
+        new Main().startGame();
     }
 }
--- dragon_cave/src/test/MainTest.java
+++ dragon_cave/src/test/MainTest.java
@@ -1,12 +1,84 @@
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class MainTest {
+import org.junit.Assert;
+import org.junit.jupiter.api.*;
 
-    @org.junit.jupiter.api.BeforeEach
+class MainTest {
+    Main main;
+
+    // runs before each method
+    @BeforeEach
     void setUp() {
+        main = new Main();
     }
 
-    @org.junit.jupiter.api.AfterEach
+    @Test
+    void introTest(){
+        assertEquals(
+                "\nYou are in a land full of dragons. In front of you,\n" +
+                        "you see two caves. In one cave, the dragon is friendly\n" +
+                        "and will share his treasure with you. The other dragon\n" +
+                        "is greedy and hungry and will eat you on sight.\n" +
+                        "Which cave will you go into? (1 or 2)\n",
+                main.intro(),
+                "intro has failed"
+        );
+    }
+
+    @Test
+    void goodDragonTest(){
+        assertEquals(
+                "\nYou approach the cave...\n" +
+                "You hear a loud thundering roar follow by a large intimidating silhouette...\n" +
+                "You then hear a loud hearty laughter as the silhouette turns into a dragon...\n" +
+                "The dragon then asked you if you wanted to be friends and offers a golden grail as a sign of your friendship.",
+                main.goodDragon(),
+                "goodDragon test failed");
+    }
+
+    @Test
+    void evilDragonTest(){
+        assertEquals(
+                "\nYou approach the cave...\n" +
+                        "It is dark and spooky...\n" +
+                        "A large dragon jumps out in front of you! He opens his jaws and...\n" +
+                        "Gobbles you down in one bite!\n",
+                main.evilDragon(),
+                "evilDragon test failed");
+    }
+
+    @Test
+    void isLostTest(){
+        assertEquals(
+                "\nYou made an invalid choice...\n" +
+                        "You wander around endlessly in the dungeon...\n" +
+                        "You became hungry and start hallucinating...\n" +
+                        "As you collapse from exhaustion, your eyes slowly closes seeing a large silhouette laughing at you.",
+                main.isLost(),
+                "isLost test failed"
+        );
+    }
+
+    @Test
+    void getChoice1Test(){
+        assertEquals(main.evilDragon(),  main.getChoice(1),"Choice 1 failed");
+    }
+
+    @Test
+    void getChoice2Test(){
+        assertEquals(main.goodDragon(),  main.getChoice(2),"Choice 2 failed");
+    }
+
+    @Test
+    void getChoice3Test(){
+        assertEquals(main.isLost(),  main.getChoice(3),"Choice 3 failed");
+    }
+
+    // run after each method
+    @AfterEach
     void tearDown() {
     }
+
+
 }
