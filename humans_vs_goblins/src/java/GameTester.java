42eaf0d3d0803c6a039f24aa6a0148401f7cf1b3
Vi Nguyen <fluffywin@gmail.com>
2022-01-10 10:27:03 -0500

    testing map

--- humans_vs_goblins/src/java/GameTester.java
+++ humans_vs_goblins/src/java/GameTester.java
@@ -1,11 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class GameTester {
 
     public static void main(String[] args) {
+        ArrayList<Land> map = new ArrayList<Land>();
         Goblins g = new Goblins();
         Humans h = new Humans();
-        Land l = new Land();
+        HashMap<String, Integer> nav = new HashMap<>();
 
-        System.out.println(g + "\n" + h + "\n" + l);
+        map.add( new Land("Great Plateau" , false, 1 , -1, -1 , -1 ) );
+        map.add( new Land("Boss Room", true, -1, 0, -1, -1 ) );
+
+        nav.put("north", 0);
+        nav.put("south", 0);
+        nav.put("east", 0);
+        nav.put("west", 0);
+
+
+        System.out.println(g + "\n" + h + "\n" + map);
 
     }
 }
--- humans_vs_goblins/src/java/Land.java
+++ humans_vs_goblins/src/java/Land.java
@@ -1,15 +1,54 @@
 public class Land {
     private String name;
-    private boolean treasure;
+    private boolean hasTreasure;
+    private int n,s,e,w;
 
-    public Land(String name, boolean treasure){
+
+    public Land(String name, boolean hasTreasure, int north, int south, int east, int west){
         this.name = name;
-        this.treasure = treasure;
+        this.hasTreasure = hasTreasure;
+        this.n = north;
+        this.s = south;
+        this.e = east;
+        this.w = west;
+
     }
 
     public Land(){
         this.name = "nameless";
-        this.treasure = false;
+        this.hasTreasure = false;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public int getS() {
+        return s;
+    }
+
+    public void setS(int s) {
+        this.s = s;
+    }
+
+    public int getE() {
+        return e;
+    }
+
+    public void setE(int e) {
+        this.e = e;
+    }
+
+    public int getW() {
+        return w;
+    }
+
+    public void setW(int w) {
+        this.w = w;
     }
 
     public String getName() {
@@ -20,19 +59,19 @@ public class Land {
         this.name = name;
     }
 
-    public boolean isTreasure() {
-        return treasure;
+    public boolean getHasTreasure() {
+        return hasTreasure;
     }
 
-    public void setTreasure(boolean treasure) {
-        this.treasure = treasure;
+    public void setHasTreasure(boolean hasTreasure) {
+        this.hasTreasure = hasTreasure;
     }
 
     @Override
     public String toString() {
         return "Land {" +
                 "name = '" + name + '\'' +
-                ", treasure = " + treasure +
+                ", treasure = " + hasTreasure +
                 '}';
     }
 }
