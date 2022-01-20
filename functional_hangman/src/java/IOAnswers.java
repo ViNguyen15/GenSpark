88315681116cadb54a8a84679d32e001e89d95e3
Vi Nguyen <fluffywin@gmail.com>
2022-01-20 12:04:34 -0500

    test cases has been implemented

--- functional_hangman/src/java/IOAnswers.java
+++ functional_hangman/src/java/IOAnswers.java
@@ -27,64 +27,66 @@ public class IOAnswers {
         }
     }
 
-    // grab arts for hangman
+    // grab arts for hangman from hangman_full_art
     static {
         try {
-            art = new ArrayList<String>() {
-                {
-                    add(Files.lines(Paths.get("src/resources/hangman/hangman0.txt"))
-                            .collect(Collectors.joining(System.lineSeparator())));
-
-                    add(Files.lines(Paths.get("src/resources/hangman/hangman1.txt"))
-                            .collect(Collectors.joining(System.lineSeparator())));
-
-                    add(Files.lines(Paths.get("src/resources/hangman/hangman2.txt"))
-                            .collect(Collectors.joining(System.lineSeparator())));
-                    add(Files.lines(Paths.get("src/resources/hangman/hangman3.txt"))
-                            .collect(Collectors.joining(System.lineSeparator())));
-                    add(Files.lines(Paths.get("src/resources/hangman/hangman4.txt"))
-                            .collect(Collectors.joining(System.lineSeparator())));
-                    add(Files.lines(Paths.get("src/resources/hangman/hangman5.txt"))
-                            .collect(Collectors.joining(System.lineSeparator())));
-                    add(Files.lines(Paths.get("src/resources/hangman/hangman6.txt"))
-                            .collect(Collectors.joining(System.lineSeparator())));
-                    add(Files.lines(Paths.get("src/resources/hangman/hangman7.txt"))
-                            .collect(Collectors.joining(System.lineSeparator())));
+            art = IntStream.range(0, Files.readAllLines( Paths.get("src/resources/hangman/hangman_full_art.txt") ).size()/5 )
+            .mapToObj(x -> {
+                try {
+                    return Files.readAllLines( Paths.get("src/resources/hangman/hangman_full_art.txt") )
+                            .subList( (x*5), (x+1)*5 );
+                } catch (IOException e) {
+                    return new ArrayList<String>(){{ add("error"); }};
                 }
-            };
-        } catch (IOException e){
+            })
+                    .collect(Collectors.toList())
+                    .stream()
+                    .map( s -> s
+                            .stream()
+                            .collect( Collectors.joining( System.lineSeparator() ) ) )
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        System.out.println(
 
-        );
+    // grab arts for hangman //legacy code
+//    static {
+//        try {
+//            art = new ArrayList<String>() {
+//                {
+//                    add(Files.lines(Paths.get("src/resources/hangman/hangman0.txt"))
+//                            .collect(Collectors.joining(System.lineSeparator())));
+//                    add(Files.lines(Paths.get("src/resources/hangman/hangman1.txt"))
+//                            .collect(Collectors.joining(System.lineSeparator())));
+//                    add(Files.lines(Paths.get("src/resources/hangman/hangman2.txt"))
+//                            .collect(Collectors.joining(System.lineSeparator())));
+//                    add(Files.lines(Paths.get("src/resources/hangman/hangman3.txt"))
+//                            .collect(Collectors.joining(System.lineSeparator())));
+//                    add(Files.lines(Paths.get("src/resources/hangman/hangman4.txt"))
+//                            .collect(Collectors.joining(System.lineSeparator())));
+//                    add(Files.lines(Paths.get("src/resources/hangman/hangman5.txt"))
+//                            .collect(Collectors.joining(System.lineSeparator())));
+//                    add(Files.lines(Paths.get("src/resources/hangman/hangman6.txt"))
+//                            .collect(Collectors.joining(System.lineSeparator())));
+//                    add(Files.lines(Paths.get("src/resources/hangman/hangman7.txt"))
+//                            .collect(Collectors.joining(System.lineSeparator())));
+//                }
+//            };
+//        } catch (IOException e){
+//            e.printStackTrace();
+//        }
+//    }
+
+    public static void main(String[] args) throws IOException {
 //        System.out.println(
 //                maxProfit(2,Arrays.asList(7,3,1,5,1,-3))
 //        );
-        var hangArt = new ArrayList<String>();
-
-        IntStream.range(0, Files.readAllLines( Paths.get("src/resources/hangman/hangman_full_art.txt") ).size()/5 )
-                        .mapToObj(x -> {
-                            try {
-                                return Files.readAllLines(Paths.get("src/resources/hangman/hangman_full_art.txt") ).subList( (x*5), (x+1)*5);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-
-                                return new ArrayList<String>(){{
-                                    add("hi");
-                                }};
-                            }
-                        }).forEach(System.out::println);
-
-        Files.readAllLines(Paths.get("src/resources/hangman/hangman0.txt"));
 
         //IntStream.range(0,Files.lines(Paths.get("src/resources/hangman/hangman0.txt") ) )
 
         //IntStream.range(0,11).map(x -> x * 10).forEach(n -> System.out.print(n + " "));
-
 
     }
 }
--- functional_hangman/src/test/MainTest.java
+++ functional_hangman/src/test/MainTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,13 +12,51 @@ class MainTest {
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
         m = new Main();
-        m.answer = "";
+        m.answer = "test";
         m.toCheck = new ArrayList<>( m.stealthStrArr( m.answer ) );
-        m.count = 0;
+        m.count = 2;
     }
 
     @Test
-    public void results(){
+    public void resultsTest(){
+        assertEquals("correct",m.results("t"),"Error, results failed");
+    }
+
+    @Test
+    public void revealLetterTest(){
+        m.revealLetter("t");
+        assertEquals(new ArrayList<String>(){{
+            add("t");
+            add("_");
+            add("_");
+            add("t");
+        }}, m.toCheck, "Error, revealLetter failed"
+        );
+    }
+
+    @Test
+    public void hasWonTest(){
+        m.revealLetter("t");
+        m.revealLetter("e");
+        m.revealLetter("s");
+        assertEquals(true, m.hasWon(), "Error, hasWon failed");
 
     }
+
+    @Test
+    public void hasLostTest(){
+        m.count = 7;
+        assertEquals(true, m.hasLost(), "Error, hasLost failed");
+    }
+
+    @Test
+    public void stealthStrArrTest(){
+        assertEquals(
+                Arrays.asList(new String[]{"_","_","_","_","_","_",})
+                , m.stealthStrArr("random")
+                , "Error stealthStrArr failed"
+        );
+
+    }
+
 }
